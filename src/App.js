cab8b1247c8ab3d04661acf5a38d99735dc1b7e6
Eduardo Valencia <EduardoVG@proton.me>
2023-08-31 20:33:04 -0600

    Primer componente

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 console.log(Header)
 function App() {
-  let suma = 3 + 8
   return (
-    <div className="App">
-      {Header()}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo con React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+    <div>
+      <Header />
+        
     </div>
   );
 }
--- src/components/Header/Header.js
+++
@@ -1,5 +0,0 @@
-const Header = () => {
-    return <img src="/img/Header.png" alt="Org" ></img>
-}
-
-export default Header()
