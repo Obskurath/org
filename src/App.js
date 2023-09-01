3b128e47f0b83fbf6dab70b121d141b76089286d
Eduardo Valencia <EduardoVG@proton.me>
2023-09-01 01:23:41 -0600

    Switch COMPLETADO

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Form from './components/Form/Form';
 import MiOrg from './components/MiOrg';
 
 function App() {
+  const [mostrarFormulario,actualizarMostrar] = useState(true)
+
+  // Ternario --> condicion ? seMuestra : noSeMuestra
+  // condicion && seMuestra
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario)
+  }
+
   return (
     <div>
       <Header />
-      <Form />
-      <MiOrg />
+      {/* {mostrarFormulario ? <Form /> : <></>}   */}
+      {mostrarFormulario && <Form />}
+      <MiOrg cambiarMostrar={cambiarMostrar}/>
     </div>
   );
 }
--- src/components/MiOrg/index.jsx
+++ src/components/MiOrg/index.jsx
@@ -1,22 +1,21 @@
 import { useState } from "react";
 import "./MiOrg.css"
 
-const MiOrg = () => {
+const MiOrg = (props) => {
     // Estado -> Hooks
     //hook -> useState
     //useState 
-    console.log()
-    //const[nombreVariable,funcionActualiza]
-    const [mostrar, actualizarMostrar] = useState(true)
-    
-    const manejarClick = () => {
-        console.log("Mostrar/Ocultar elemento", !mostrar)
-        actualizarMostrar(!mostrar)
-    }
+    //const[nombreVariable,funcionActualiza] = useState(valorInicial)
+    console.log(props)
+    // const [mostrar, actualizarMostrar] = useState(true)
+    // const manejarClick = () => {
+    //     console.log("Mostrar/Ocultar elemento", !mostrar)
+    //     actualizarMostrar(!mostrar)
+    // }
 
     return <section className="orgSection">
         <h3 className="title">Mi Organización</h3>
-        <img src="/img/add.png" alt="Expandir formulario" onClick={manejarClick}/>
+        <img src="/img/add.png" alt="Expandir formulario" onClick={props.cambiarMostrar}/>
     </section>
 };
 
