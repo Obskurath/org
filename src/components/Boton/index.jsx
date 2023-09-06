d8d3fbd22ab494dce4b0985f0cd5b5b73f119860
Eduardo Valencia <EduardoVG@proton.me>
2023-09-05 22:42:33 -0600

    Clase 04 Completada

--- src/components/Boton/index.jsx
+++
@@ -1,7 +0,0 @@
-import "./Boton.css"
-
-const Boton = (props) => {
-    return <button className="btn">{props.children}</button>
-}
-
-export default Boton;
--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import "./Header.css"
-
-const Header = () => {
-    return <header className="header">
-        <img src="/img/Header.png" alt="Org" />
-    </header>
-};
-
-export default Header;
