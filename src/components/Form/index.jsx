1b0d1e3640fb4b59bf71cd3da6878dad512ea66c
Eduardo Valencia <EduardoVG@proton.me>
2024-04-12 22:36:51 -0600

    Form component css and form fields

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,12 +1,16 @@
 import "./Form.css";
 import TextField from "../TextField";
+import OptionList from "../OptionList";
 
 const Form = () => {
   return (
     <section className="form">
       <form action="">
         <h2>Rellena el formulario de para crear el colaborador.</h2>
-        <TextField className="Name" />
+        <TextField title="Nombre" placeholder="Ingresar su nombre" />
+        <TextField title="Puesto" placeholder="Ingresar su puesto" />
+        <TextField title="Foto" placeholder="Ingresar link de la foto" />
+        <OptionList title="Equipo" placeholder="Seleccione su equipo" />
       </form>
     </section>
   );
