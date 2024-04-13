939cb09085f00c19afb0c6bd7720c6dc616b1beb
Eduardo Valencia <EduardoVG@proton.me>
2024-04-13 01:38:40 -0600

    Added transition to form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import MiOrg from "./components/MiOrg";
 
 function App() {
+  const [showForm, updateShow] = useState(true);
+
+  //Ternario => conidcion ? seMuestra : noSeMuestra
+
+  const changeShow = () => {
+    updateShow(!showForm);
+  };
+
   return (
     <div>
       <Header />
-      <Form />
+      <div className={`form-container ${showForm ? "show" : "hide"}`}>
+        <Form />
+      </div>
+      <MiOrg changeShow={changeShow} />
     </div>
   );
 }
