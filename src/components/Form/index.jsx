fdf47ef8bd30ac647ea88c31041d5bd650de13ae
Eduardo Valencia <EduardoVG@proton.me>
2024-04-12 23:30:55 -0600

    Prevent default

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,16 +1,22 @@
 import "./Form.css";
 import TextField from "../TextField";
 import OptionList from "../OptionList";
+import Button from "../Button";
 
 const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className="form">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h2>Rellena el formulario de para crear el colaborador.</h2>
-        <TextField title="Nombre" placeholder="Ingresar su nombre" />
-        <TextField title="Puesto" placeholder="Ingresar su puesto" />
-        <TextField title="Foto" placeholder="Ingresar link de la foto" />
+        <TextField title="Nombre" placeholder="Ingresar su nombre" required />
+        <TextField title="Puesto" placeholder="Ingresar su puesto" required />
+        <TextField title="Foto" placeholder="Ingresar link de foto" required />
         <OptionList title="Equipo" placeholder="Seleccione su equipo" />
+        <Button title="Crear" />
       </form>
     </section>
   );
