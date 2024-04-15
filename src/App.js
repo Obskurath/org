9a44f9476e258c9c4b63df81b942e2f2e700d1b6
Eduardo Valencia <EduardoVG@proton.me>
2024-04-15 01:00:32 -0600

    Add UUID to partners and teams in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
@@ -14,36 +15,42 @@ function App() {
       photo: "https://github.com/obskurath.png",
       name: "Eduardo Valencia",
       job: "Estudiante",
+      id: uuidv4(),
     },
     {
       team: "Front End",
       photo: "https://github.com/harlandlohora.png",
       name: "Harland Lohora",
       job: "Instructor",
+      id: uuidv4(),
     },
     {
       team: "Programación",
       photo: "https://github.com/JeanmarieAluraLatam.png",
       name: "Jean Marie",
       job: "Desarrolladora de software e instructora",
+      id: uuidv4(),
     },
     {
       team: "UX y Diseño",
       photo: "https://github.com/JeanmarieAluraLatam.png",
       name: "Jeanmarie Quijada",
       job: "Instructora en Alura Latam",
+      id: uuidv4(),
     },
     {
       team: "Programación",
       photo: "https://github.com/christianpva.png",
       name: "Christian Velasco",
       job: "Head de Alura e Instructor",
+      id: uuidv4(),
     },
     {
       team: "Innovación y Gestión",
       photo: "https://github.com/JoseDarioGonzalezCha.png",
       name: "Jose Gonzalez",
       job: "Dev FullStack",
+      id: uuidv4(),
     },
   ]);
 
@@ -61,8 +68,10 @@ function App() {
   };
 
   // Remove partner
-  const removePartner = () => {
-    console.log("Remove partner");
+  const removePartner = (id) => {
+    console.log("Remove partner", id);
+    const newPartners = partners.filter((partner) => partner.id !== id);
+    updatePartners(newPartners);
   };
 
   // Team List
@@ -71,36 +80,43 @@ function App() {
       title: "Programación",
       primaryColor: "#57C278",
       secondaryColor: "#D9F7E9",
+      id: uuidv4(),
     },
     {
       title: "Front End",
       primaryColor: "#82CFFA",
       secondaryColor: "#E8F8FF",
+      id: uuidv4(),
     },
     {
       title: "Data Science",
       primaryColor: "#A6D157",
       secondaryColor: "#F0F8E2",
+      id: uuidv4(),
     },
     {
       title: "DevOps",
       primaryColor: "#E06B69",
       secondaryColor: "#FDE7E8",
+      id: uuidv4(),
     },
     {
       title: "UX/UI",
       primaryColor: "#DB6EBF",
       secondaryColor: "#FAE9F5",
+      id: uuidv4(),
     },
     {
       title: "Móvil",
       primaryColor: "#FFBA05",
       secondaryColor: "#FFF5D9",
+      id: uuidv4(),
     },
     {
       title: "Innovación y Gestión",
       primaryColor: "#FF8A29",
       secondaryColor: "#FFEEDF",
+      id: uuidv4(),
     },
   ];
 
